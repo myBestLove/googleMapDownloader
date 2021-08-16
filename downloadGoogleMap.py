d8c4689d8354033c410ad1828e3410f2320099b1
myBestLove <h1391531266@163.com>
2021-08-16 23:45:40 +0800

    上传基础下载

---
+++ downloadGoogleMap.py
@@ -0,0 +1,380 @@
+import os
+import io
+import sys
+import random
+import re
+import math
+import shapefile
+from threading import Thread, Lock
+import urllib.request as ur
+import numpy as np
+from matplotlib import pyplot as plt
+from osgeo import gdal
+
+from PIL import Image
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+Image.MAX_IMAGE_PIXELS = None
+
+
+MAP_URLS = {
+    "google": "http://mt{server}.google.cn/vt/lyrs={style}&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}",
+    "amap": "http://wprd02.is.autonavi.com/appmaptile?style={style}&x={x}&y={y}&z={z}",
+    "tencent_s": "http://p3.map.gtimg.com/sateTiles/{z}/{fx}/{fy}/{x}_{y}.jpg",
+    "tencent_m": "http://rt0.map.gtimg.com/tile?z={z}&x={x}&y={y}&styleid=3"
+}
+
+
+COUNT = 0
+mutex = Lock()
+
+# ------------------wgs84与web墨卡托互转-------------------------
+
+# WGS-84经纬度转Web墨卡托
+
+
+def wgs_to_macator(x, y):
+    y = 85.0511287798 if y > 85.0511287798 else y
+    y = -85.0511287798 if y < -85.0511287798 else y
+
+    x2 = x * 20037508.34 / 180
+    y2 = math.log(math.tan((90 + y) * math.pi / 360)) / (math.pi / 180)
+    y2 = y2 * 20037508.34 / 180
+    return x2, y2
+
+# Web墨卡托转经纬度
+
+
+def mecator_to_wgs(x, y):
+    x2 = x / 20037508.34 * 180
+    y2 = y / 20037508.34 * 180
+    y2 = 180 / math.pi * \
+        (2 * math.atan(math.exp(y2 * math.pi / 180)) - math.pi / 2)
+    return x2, y2
+
+# -------------------------------------------------------------
+
+
+# ---------------------瓦片地址到墨卡托---------------------------------
+'''
+东经为正，西经为负。北纬为正，南纬为负
+lon经度 lat纬度 z缩放比例[0-22] ,对于卫星图并不能取到最大，测试值是20最大，再大会返回404.
+山区卫星图可取的z更小，不同地图来源设置不同。
+'''
+# 根据WGS-84 的经纬度获取谷歌地图中的瓦片坐标
+
+
+def wgs84_to_tile(lon, lat, z):
+    '''
+    gps              tile
+         ^              -------->
+         |              |
+    -----|---->  =>>    |
+         |              v
+
+    Get google-style tile cooridinate from geographical coordinate
+    lon : Longittude
+    lat : Latitude
+    z : zoom
+    '''
+    def isnum(x): return isinstance(x, int) or isinstance(x, float)
+    if not(isnum(lon) and isnum(lat)):
+        raise TypeError("lon and lat must be int or float!")
+
+    if not isinstance(z, int) or z < 0 or z > 22:
+        raise TypeError("z must be int and between 0 to 22.")
+
+    if lon < 0:
+        lon = 180 + lon
+    else:
+        lon += 180
+    lon /= 360  # make lon to (0,1)
+
+    lat = 85.0511287798 if lat > 85.0511287798 else lat
+    lat = -85.0511287798 if lat < -85.0511287798 else lat
+    lat = math.log(math.tan((90 + lat) * math.pi / 360)) / (math.pi / 180)
+    lat /= 180  # make lat to (-1,1)
+    lat = 1 - (lat + 1) / 2  # make lat to (0,1) and left top is 0-point
+
+    num = 2**z
+    x = math.floor(lon * num)
+    y = math.floor(lat * num)
+    return x, y
+
+
+def tile_to_mercator(tile_x, tile_y, z):
+    length = 20037508.3427892
+    sum = 2**z
+    mercator_x = tile_x / sum * length * 2 - length
+    mercator_y = -(tile_y / sum * length * 2) + length
+
+    return mercator_x, mercator_y
+
+# -----------------------------------------------------------
+
+
+# ---------------------度分秒转换------------------------------
+
+def dms2dd(degrees, minutes, seconds, direction):
+
+    dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60)
+
+    if direction == 'E' or direction == 'N':
+
+        dd *= -1
+
+    return dd
+
+
+def dd2dms(deg):
+
+    d = int(deg)
+
+    md = abs(deg - d) * 60
+
+    m = int(md)
+
+    sd = (md - m) * 60
+
+    return [d, m, sd]
+
+
+def parse_dms(dms):
+
+    parts = re.split('[^\d\w]+', dms)
+
+    lat = dms2dd(parts[0], parts[1], parts[2], parts[3])
+
+    return (lat)
+
+
+# ---------------------下载器相关-----------------------------
+agents = [
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/532.9 (KHTML, like Gecko) Chrome/5.0.310.0 Safari/532.9',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.514.0 Safari/534.7',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/9.0.601.0 Safari/534.14',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/10.0.601.0 Safari/534.14',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.20 (KHTML, like Gecko) Chrome/11.0.672.2 Safari/534.20',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1'
+]
+
+
+class Downloader(Thread):
+    # multiple threads downloader
+    def __init__(self, index, count, urls, datas, update):
+        # index 表示第几个线程，count 表示线程的总数，urls 代表需要下载url列表，datas代表要返回的数据列表。
+        # update 表示每下载一个成功就进行的回调函数。
+        super().__init__()
+        self.urls = urls
+        self.datas = datas
+        self.index = index
+        self.count = count
+        self.update = update
+        self.pos = None
+
+    def download(self, url):
+        HEADERS = {'User-Agent': random.choice(agents)}
+        # print(url)
+
+        err = 0
+        while(err < 4):
+            full_url = url.format(server=err)  # 如果下载失败，则切换服务器
+            header = ur.Request(full_url, headers=HEADERS)
+            try:
+                # print('download ', full_url)
+                data = ur.urlopen(header).read()
+            except:
+                err += 1
+                print("Bad network link.", full_url)
+            else:
+                return self.pos, data
+        # raise Exception("Bad network link.")
+        print("Bad network link.", url)
+        return None
+
+    def run(self):
+        for i, (x, y, url) in enumerate(self.urls):
+            if i % self.count != self.index:
+                continue
+            self.pos = [x, y]
+            self.datas[i] = self.download(url)
+            if mutex.acquire():
+                self.update()
+                mutex.release()
+
+
+def downTiles(urls, multi=10):
+
+    def makeupdate(s):
+        def up():
+            global COUNT
+            COUNT += 1
+            print("\b"*45, end='')
+            print("DownLoading ... [{0}/{1}]".format(COUNT, s), end='')
+        return up
+
+    url_len = len(urls)
+    datas = [None] * url_len
+    if multi < 1 or multi > 20 or not isinstance(multi, int):
+        raise Exception(
+            "multi of Downloader shuold be int and between 1 to 20.")
+    tasks = [Downloader(i, multi, urls, datas, makeupdate(url_len))
+             for i in range(multi)]
+    for i in tasks:
+        i.start()
+    for i in tasks:
+        i.join()
+
+    return datas
+
+
+def geturl(source, x, y, z, style):
+    '''
+    Get the picture's url for download.
+    style:
+        m for map
+        s for satellite
+    source:
+        google or amap or tencent
+    x y:
+        google-style tile coordinate system
+    z:
+        zoom
+    '''
+    if source == 'google':
+        furl = MAP_URLS["google"].format(
+            server='{server}', x=x, y=y, z=z, style=style)
+    elif source == 'amap':
+        # for amap 6 is satellite and 7 is map.
+        style = 6 if style == 's' else 7
+        furl = MAP_URLS["amap"].format(x=x, y=y, z=z, style=style)
+    elif source == 'tencent':
+        y = 2**z - 1 - y
+        if style == 's':
+            furl = MAP_URLS["tencent_s"].format(
+                x=x, y=y, z=z, fx=math.floor(x / 16), fy=math.floor(y / 16))
+        else:
+            furl = MAP_URLS["tencent_m"].format(x=x, y=y, z=z)
+    else:
+        raise Exception("Unknown Map Source ! ")
+
+    return furl
+
+
+def getTilesByBBox(bbox, zoom):
+    '''
+    bbox依次输入左上角的经度、纬度，右下角的经度、纬度，缩放级别，地图源，输出文件，影像类型（默认为卫星图）
+    获取区域内的瓦片并自动拼合图像。返回四个角的瓦片坐标
+    '''
+    x1, y1, x2, y2 = bbox
+    pos1x, pos1y = wgs84_to_tile(x1, y1, zoom)
+    pos2x, pos2y = wgs84_to_tile(x2, y2, zoom)
+    lenx = pos2x - pos1x + 1
+    leny = pos2y - pos1y + 1
+    print("Total number：{x} X {y}".format(x=lenx, y=leny))
+
+    tiles = []
+    for y in range(pos1y, pos2y+1):
+        for x in range(pos1x, pos2x+1):
+            tiles.append([x, y])
+
+    return tiles, [pos1x, pos1y, pos2x, pos2y], [leny*256, lenx*256]
+
+
+def getUrlsByTiles(tiles, tile_bbox, zoom, source='google', style='s'):
+    pos1x, pos1y, pos2x, pos2y = tile_bbox
+    urls = []
+    for x, y in tiles:
+        url = geturl(source, x, y, zoom, style)
+        urls.append([x-pos1x, y-pos1y, url])
+    return urls
+
+
+def getExtent(bbox, zoom, mode='tile'):
+    if mode == 'tile':
+        mercator_x1, mercator_y1 = tile_to_mercator(
+            bbox[0], bbox[1], zoom)
+        mercator_x2, mercator_y2 = tile_to_mercator(
+            bbox[2]+1, bbox[3]+1, zoom)
+    elif mode == 'wgs84':
+        mercator_x1, mercator_y1 = wgs_to_macator(
+            bbox[0], bbox[1], zoom)
+        mercator_x2, mercator_y2 = wgs_to_macator(
+            bbox[2]+1, bbox[3]+1, zoom)
+    else:
+        print("get extend error")
+        return None
+    return [mercator_x1, mercator_y1, mercator_x2, mercator_y2]
+
+
+def getTransform(mercator_bbox, image_shape):
+    height, width = image_shape
+    mercator_x1, mercator_y1, mercator_x2, mercator_y2 = mercator_bbox
+
+    res_x = (mercator_x2 - mercator_x1) / width
+    res_y = (mercator_y2 - mercator_y1) / height
+
+    trans = [mercator_x1, res_x, 0,
+             mercator_y1, 0, res_y]
+    return trans
+
+
+def saveTif(datas, im_geotrans, image_shape, outfile):
+    # 创建文件
+    height, width = image_shape
+    datatype = gdal.GDT_Byte
+    driver = gdal.GetDriverByName("GTiff")
+    dataset = driver.Create(outfile, width, height, 3, datatype)
+    if(dataset != None):
+        dataset.SetGeoTransform(im_geotrans)  # 写入仿射变换参数
+        # dataset.SetProjection(im_proj) #写入投影
+
+        for index, result in enumerate(datas):
+            if result is None:
+                continue
+
+            pos, data = result
+            x, y = pos
+            picio = io.BytesIO(data)
+            picpil = Image.open(picio)
+            data = np.array(picpil, dtype=np.uint8)
+            # print(pos, data.shape)
+            # print(x*256, y*256)
+            _, _, bands = data.shape
+            for c in range(bands):
+                dataset.GetRasterBand(
+                    c + 1).WriteArray(data[:, :, c], x*256, y*256)
+
+    del dataset
+
+
+def downloadRectDemo():
+    source = 'google'
+    style = 's'
+    zoom = 18
+    geo_name = 'test'
+    gps_bbox = [120.09198099000002, 27.176422685000115,
+                120.09819027500012, 27.173431772500086]
+
+    tiles, tile_bbox, image_shape = getTilesByBBox(gps_bbox, zoom)
+    urls = getUrlsByTiles(tiles, tile_bbox, zoom, source, style)
+
+    mercator_bbox = getExtent(tile_bbox, zoom, mode='tile')
+    trans = getTransform(mercator_bbox, image_shape)
+    print(trans)
+    # print(image_shape)
+    datas = downTiles(urls)
+
+    print("\nDownload Finished！ Pics (w,h) is (%d,%d) Mergeing......" %
+          (image_shape[0], image_shape[1]))
+
+    outfile = '{}_{}{}_{}.tif'.format(source, zoom, style, geo_name)
+    saveTif(datas, trans, image_shape, outfile)
+
+
+if __name__ == "__main__":
+    downloadRectDemo()
